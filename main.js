634cc1f240b93a0b8a16bb923ef4033c2cf61022
Stevie Ambroise <s.ambroise@protonmail.com>
2023-12-19 10:38:14 -0700

    Move radiobuttons in form and add button disabling if nothing is checked

--- main.js
+++ main.js
@@ -28,14 +28,15 @@ let mantras = [
   "May all beings everywhere be happy and free, and may the thoughts, words, and actions of my own life contribute to the happiness and freedom for all.",
   "Expect nothing and appreciate everything.",
   "Love and kindness guide my actions, creating a world filled with compassion.",
-  'Divine love flows through me, offering comfort and healing to those in need.'
+  "Divine love flows through me, offering comfort and healing to those in need."
 ]
 
 const getRandomIndex = (array) => {
   return Math.floor(Math.random() * array.length);
 }
 
-const showUserChoice = () => {
+const showUserChoice = (e) => {
+  e.preventDefault()
   hide([meditateImage])
   show([clearLink])
   userMessage.classList.remove('error-message')
@@ -56,31 +57,59 @@ const clearMessage = () => {
   clearLink.classList.add('hidden')
 }
 
-const addOwnMessage = () => {
-  formTitle.textContent = "What type of message are you entering?"
-  show([userInputField, submitInputButton])
-  hide([meditateImage, receiveButton, userMessage])
-  let noChoiceSelected = !affirmRadioButton.checked || !mantraRadioButton.checked
+const addOwnMessageToDataSet = () => {
   if(affirmRadioButton.checked) {
-    show([userMessage])
-    userMessage.classList.remove('error-message')
-    affirmations.push(userInputField.value)
-    userMessage.textContent = userInputField.value
-    userInputField.value && hide([userInputField, submitInputButton])
-    userInputField.value && show([receiveButton, clearLink])
-  } else if(mantraRadioButton.checked) {
-    show([userMessage])
-    userMessage.classList.remove('error-message')
-    mantras.push(userInputField.value)
-    userMessage.innerHTML = userInputField.value
-    userInputField.value && hide([userInputField, submitInputButton])
-    userInputField.value && show([receiveButton, clearLink])
-  } else if(noChoiceSelected) {
+    affirmations.push(userInputField.value !== "" && userInputField.value)
+  } else {
+    mantras.push(userInputField.value !== "" && userInputField.value)
+  }
+}
+
+const showSubmitGemField = () => {
+  userMessage.classList.remove('error-message')
+  checkForChoiceSelection()
+  hide([meditateImage, receiveButton, userMessage])
+  show([userInputField, submitInputButton])
+  showErrorMessage()
+}
+
+
+const checkForChoiceSelection = () => {
+  let choiceSelected = affirmRadioButton.checked || mantraRadioButton.checked
+  if(!choiceSelected) {
+    submitInputButton.disabled = true
+  } else {
+    submitInputButton.disabled = false
+  }
+}
+
+const showErrorMessage = () => {
+  let noChoiceSelected = !affirmRadioButton.checked && !mantraRadioButton.checked
+  console.log(noChoiceSelected)
+  if(noChoiceSelected) {
     show([userMessage])
     userMessage.classList.add('error-message')
     userMessage.textContent = `You need to choose a message type.`
+  } else {
+    userMessage.classList.remove('error-message')
   }
 }
+
+const submitMessage = () => {
+  addOwnMessageToDataSet()
+  formTitle.textContent = "What type of message are you entering?"
+  userMessage.textContent = userInputField.value
+  show([clearLink, receiveButton, userMessage])
+  hide([userInputField, submitInputButton])
+  userInputField.textContent = ""
+}
+
+//BUG, there's an empty message in the array, after the user adds a gem.
+//Interesting.
+
+//Separation of tasks.
+//One function for adding to the array,
+//One function for showing the message
 
 const show = (elements) => {
   elements.forEach(element => element.classList.remove('hidden'));
@@ -90,14 +119,11 @@ const hide = (elements) => {
   elements.forEach(element => element.classList.add('hidden'));
 }
 
-receiveButton.addEventListener('click', showUserChoice)
+receiveButton.addEventListener('click', (e) =>  showUserChoice(e))
 clearLink.addEventListener('click', clearMessage)
-addGem.addEventListener('click', addOwnMessage)
-submitInputButton.addEventListener('click', addOwnMessage)
+addGem.addEventListener('click', showSubmitGemField)
+submitInputButton.addEventListener('click', submitMessage)
 
 
-//It needs to push the message into the affirmation or mantra array
-//It needs to... show the message immediately, and then iterate again?
-//Local storage?
-
-//Make  a clear form button, or hide the form
+//BUG
+// - The user can submit a 
